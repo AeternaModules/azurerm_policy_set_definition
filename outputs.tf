af02d2cb866a8682986a90d0c1ab0f14c906843f
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,41 @@
 output "policy_set_definitions_id" {
   description = "Map of id values across all policy_set_definitions, keyed the same as var.policy_set_definitions"
-  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.id }
+  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "policy_set_definitions_description" {
   description = "Map of description values across all policy_set_definitions, keyed the same as var.policy_set_definitions"
-  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.description }
+  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.description if v.description != null && length(v.description) > 0 }
 }
 output "policy_set_definitions_display_name" {
   description = "Map of display_name values across all policy_set_definitions, keyed the same as var.policy_set_definitions"
-  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.display_name }
+  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.display_name if v.display_name != null && length(v.display_name) > 0 }
 }
 output "policy_set_definitions_management_group_id" {
   description = "Map of management_group_id values across all policy_set_definitions, keyed the same as var.policy_set_definitions"
-  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.management_group_id }
+  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.management_group_id if v.management_group_id != null && length(v.management_group_id) > 0 }
 }
 output "policy_set_definitions_metadata" {
   description = "Map of metadata values across all policy_set_definitions, keyed the same as var.policy_set_definitions"
-  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.metadata }
+  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.metadata if v.metadata != null && length(v.metadata) > 0 }
 }
 output "policy_set_definitions_name" {
   description = "Map of name values across all policy_set_definitions, keyed the same as var.policy_set_definitions"
-  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.name }
+  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "policy_set_definitions_parameters" {
   description = "Map of parameters values across all policy_set_definitions, keyed the same as var.policy_set_definitions"
-  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.parameters }
+  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.parameters if v.parameters != null && length(v.parameters) > 0 }
 }
 output "policy_set_definitions_policy_definition_group" {
   description = "Map of policy_definition_group values across all policy_set_definitions, keyed the same as var.policy_set_definitions"
-  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.policy_definition_group }
+  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.policy_definition_group if v.policy_definition_group != null && length(v.policy_definition_group) > 0 }
 }
 output "policy_set_definitions_policy_definition_reference" {
   description = "Map of policy_definition_reference values across all policy_set_definitions, keyed the same as var.policy_set_definitions"
-  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.policy_definition_reference }
+  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.policy_definition_reference if v.policy_definition_reference != null && length(v.policy_definition_reference) > 0 }
 }
 output "policy_set_definitions_policy_type" {
   description = "Map of policy_type values across all policy_set_definitions, keyed the same as var.policy_set_definitions"
-  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.policy_type }
+  value       = { for k, v in azurerm_policy_set_definition.policy_set_definitions : k => v.policy_type if v.policy_type != null && length(v.policy_type) > 0 }
 }
 
